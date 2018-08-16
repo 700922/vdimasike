1ff303c8e582380332ab5bc4af5cbc7f3468d916
700922 <tzar0507@gmail.com>
2018-08-16 10:29:25 +0300

    #new user signup

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -41,18 +41,19 @@ AppAsset::register($this);
             //['label' => 'Домой', 'url' => ['/site/index']],
             //['label' => 'About', 'url' => ['/site/about']],
             //['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
+            if (Yii::$app->user->isGuest) {
+                $menuItems[] = ['label' => 'Регистрация', 'url' => ['/site/signup']];
+                $menuItems[] = ['label' => 'Войти', 'url' => ['/site/login']];
+            } else {
+                $menuItems[] = '<li>'
+                    . Html::beginForm(['/site/Выйти'], 'post')
+                    . Html::submitButton(
+                        'Logout (' . Yii::$app->user->identity->username . ')',
+                        ['class' => 'btn btn-link logout']
+                    )
+                    . Html::endForm()
+                    . '</li>';
+            }
         ],
     ]);
     NavBar::end();
